2717efdc22b512f5bd8565a83291788c3467293a
Oliver Hoidn <ohoidn@gmail.com>
2017-03-24 11:56:23 -0700

    Add plots of Bragg peak intensity progressions.
    To adhoc_jitter_correction.ipynb

--- config.py
+++ config.py
@@ -18,11 +18,15 @@ url = 'https://docs.google.com/spreadsheets/d/%s/edit#gid=0' % logbook_ID
 use_logbook = True
 photon_energy = 8910.0
 pulse_energy = 0.0001
-DetInfo = namedtuple('DetInfo', ['device_name',
- 'geometry',
- 'extra_masks',
- 'subregion_index'])
+DetInfo = namedtuple('DetInfo',
+    ['device_name',
+     'hdf_path',
+     'geometry',
+     'extra_masks',
+     'subregion_index'])
+
 detinfo_map = {'quad1': DetInfo('MecTargetChamber.0:Cspad.0', 
+    '/Configure:0000/Run:0000/CalibCycle:0000/CsPad2x2::ElementV1/MecTargetChamber.0:Cspad2x2.1',
     {'y0': 187, 'phi': 0, 'r': 910.4, 'x0': 409, 'alpha': 2.44874},
 #            {'phi': 0,
 #           'x0': 409,
@@ -30,20 +34,28 @@ detinfo_map = {'quad1': DetInfo('MecTargetChamber.0:Cspad.0',
 #           'alpha': 2.33874,
 #           'r': 1041.4},
             {'../masks/quad1_mask_10.11.npy'}, 0),
- 'quad2': DetInfo('MecTargetChamber.0:Cspad.0', {'phi': -1.5899543,
+            #{'../masks/quad1_mask_10.27_shrunk3.npy'}, 0),
+ 'quad2': DetInfo('MecTargetChamber.0:Cspad.0',
+    '/Configure:0000/Run:0000/CalibCycle:0000/CsPad2x2::ElementV1/MecTargetChamber.0:Cspad2x2.2',
+     {'phi': -1.5899543,
            'x0': 654.165,
            'y0': 574.192,
            'alpha': 0.759423,
            'r': 970.13}, {'../masks/quad2_mask_10.11.npy'}, 1),
- 'allquads': DetInfo('MecTargetChamber.0:Cspad.0', {'phi': None,
+           #'r': 970.13}, {'../masks/quad2_mask_10.27_shrunk3.npy'}, 1),
+ 'allquads': DetInfo('MecTargetChamber.0:Cspad.0', 
+    '/Configure:0000/Run:0000/CalibCycle:0000/CsPad::ElementV2/MecTargetChamber.0:Cspad.0',
+     {'phi': None,
               'x0': None,
               'y0': None,
               'alpha': None,
               'r': None}, {}, -1),
- 'xrts1': DetInfo('MecTargetChamber.0:Cspad2x2.1', {}, {}, -1),
- 'xrts2': DetInfo('MecTargetChamber.0:Cspad2x2.2', {}, {}, -1),
- 'vuv': DetInfo('MecTargetChamber.0:Princeton.1', {}, {}, -1),
- 'si': DetInfo('MecTargetChamber.0:Opal1000.1', {}, {}, -1)}
+ 'xrts1': DetInfo('MecTargetChamber.0:Cspad2x2.1','',  {}, {}, -1),
+ 'xrts2': DetInfo('MecTargetChamber.0:Cspad2x2.2', '', {}, {}, -1),
+ 'vuv': DetInfo('MecTargetChamber.0:Princeton.1','',  {}, {}, -1),
+ 'si': DetInfo('MecTargetChamber.0:Opal1000.1',
+     '/Configure:0000/Run:0000/CalibCycle:0000/Camera::FrameV1/MecTargetChamber.0:Opal1000.1',
+     {}, {}, -1)}
 NonareaInfo = namedtuple('NonareaInfo', ['type', 'src'])
 nonarea = {'d1': NonareaInfo('Lusi.IpmFexV1', 'MEC-TCTR-DI-01'),
  'ipm2': NonareaInfo('Lusi.IpmFexV1', 'MEC-XT2-IPM-02'),
@@ -208,23 +220,31 @@ def every_other_filter(arr, nevent = None, **kwargs):
     For use in testing mode.
     """
     return bool(nevent % 20)
-
-
 best_focus_size = 2.0
 
 def sum_window(smin, smax):
     return lambda arr: smin < np.sum(arr) < smax
 
 
-def beam_intensity_diagnostic(label):
+# TODO: make this take a dataref (rather than a mere label)
+def dataset_beam_intensity_diagnostic(dataset):
     """
-    Return average beam energy in a dataset, evaluated based on Si spectrometer spectrum.
+    return average beam energy in a dataset, evaluated based on si spectrometer spectrum.
     """
     gmd_scale_factor = 1.0
-    si_integral_to_eV = 7.192326355322434e-11
+    si_integral_to_ev = 7.192326355322434e-11
     from dataccess import mec
-    return gmd_scale_factor * si_integral_to_eV * mec.si_spectrometer_integral(label)
+    return gmd_scale_factor * si_integral_to_ev * mec.si_imarr_sum(dataset.evaluate('si').mean)
+# TODO: make this take a dataref (rather than a mere label)
 
+def beam_intensity_diagnostic(pattern):
+    """
+    return average beam energy in a dataset, evaluated based on si spectrometer spectrum.
+    """
+    gmd_scale_factor = 1.0
+    si_integral_to_ev = 7.192326355322434e-11
+    from dataccess import mec
+    return gmd_scale_factor * si_integral_to_ev * mec.si_imarr_sum(pattern.dataset.evaluate('si').mean)
 
 def xrts1_fe_fluorescence_integral(label):
     from dataccess import mec
--- masks/masks.py
+++ masks/masks.py
@@ -1,6 +1,17 @@
 import numpy as np
+from dataccess import utils
 
-def zero_islands(arr, threshold, minsize = 75):
+#@utils.memoize(timeout = None)
+def find_islands(arr, threshold, minsize = 75):
+    """
+    Remove 'islands' meeting the specified minimum size, and consisting of
+    pixels with value above the given threshold, by setting them to 0.
+
+    Returns the modified array and a list of ndarray of coordinate pairs for
+    the affected pixels for each cluster/island:
+        arr -> np.ndarray, clusters -> list of np.ndarray
+    """
+    arr = arr.copy()
     q = []
     def search(i, j):
         cluster = []
@@ -27,18 +38,29 @@ def zero_islands(arr, threshold, minsize = 75):
         for coords in cluster:
             arr[coords] = 0
     visited = np.zeros(arr.shape, dtype = np.bool)
+    clusters = []
     for i in range(arr.shape[0]):
         for j in range(arr.shape[1]):
             if (not visited[i][j]) and (arr[i][j]) > 0:
                 cluster = search(i, j)
                 if len(cluster) < minsize:
                     zero_cluster(cluster)
-    return arr
+                else:
+                    clusters.append(cluster)
+    return arr, map(np.array, clusters)
 
-m1 = np.load('quad1_mask_calibman_10.12.npy')
+#@utils.memoize(timeout = None)
+def bounding_view(array, cluster):
+    def get_bounds(cluster):
+        i, j = cluster.T
+        return np.min(i), np.max(i), np.min(j), np.max(j)
+    imin, imax, jmin, jmax = get_bounds(cluster)
+    return array[imin:imax + 1, jmin:jmax + 1]
 
-m2 = np.load('quad2_mask_calibman_10.12.npy')
-#m2 = np.load('mask3.npy')
+def clustermap(arr, clusters, func):
+    def do_one(cluster):
+        return bounding_view(arr, cluster).copy()
+    return map(func, map(do_one, clusters))
 
 def outlier_mask(arr, min = -10, max = 10):
     mask = np.ones(arr.shape, dtype = bool)
@@ -51,13 +73,20 @@ def grid_mask(arr, stride = 10):
     mask[:, ::stride] = False
     return mask
 
-dark1 = np.load('dark1.npy').T
-new1 = outlier_mask(dark1) * m1
-#new1 = zero_islands(outlier_mask(dark1) * grid_mask(dark1) * m1, 0, 75)
+def write_masks():
+    m1 = np.load('quad1_mask_calibman_10.12.npy')
 
-dark2 = np.load('dark2.npy').T
-new2 = outlier_mask(dark2) * m2
-#new2 = zero_islands(outlier_mask(dark2) * grid_mask(dark2) * m2, 0, 75)
+    m2 = np.load('quad2_mask_calibman_10.12.npy')
+    #m2 = np.load('mask3.npy')
 
-np.save('quad1_mask_10.11.npy', new1)
-np.save('quad2_mask_10.11.npy', new2)
+    dark1 = np.load('dark1.npy').T
+    new1 = outlier_mask(dark1) * m1
+    #new1 = zero_islands(outlier_mask(dark1) * grid_mask(dark1) * m1, 0, 75)
+
+    dark2 = np.load('dark2.npy').T
+    new2 = outlier_mask(dark2) * m2
+    #new2 = zero_islands(outlier_mask(dark2) * grid_mask(dark2) * m2, 0, 75)
+
+    np.save('quad1_mask_10.11.npy', new1)
+
+    np.save('quad2_mask_10.11.npy', new2)
